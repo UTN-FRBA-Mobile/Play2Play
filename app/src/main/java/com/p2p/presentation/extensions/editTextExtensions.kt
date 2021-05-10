2deacf7b0f7dde5c7ab1bd8586a5e7d7c032fe81
Dylan Muszel <dylan.muszel@mercadolibre.com>
2021-05-09 23:11:43 -0300

    Fix npe

--- app/src/main/java/com/p2p/presentation/extensions/editTextExtensions.kt
+++ app/src/main/java/com/p2p/presentation/extensions/editTextExtensions.kt
@@ -5,5 +5,5 @@ import android.widget.EditText
 /** Clears the text and append a new one. It's useful to keep the cursor at the end of the input. */
 fun EditText.clearAndAppend(text: String?) {
     setText(null)
-    append(text)
+    append(text.orEmpty())
 }
