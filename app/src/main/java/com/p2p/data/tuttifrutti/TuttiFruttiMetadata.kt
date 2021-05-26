851014cffbe716afb3827d71b4fcb2ca503bb2f8
brendaboryszanski <brenda.boryszanski@despegar.com>
2021-05-26 18:21:30 -0300

    Update app/src/main/java/com/p2p/data/tuttifrutti/TuttiFruttiMetadata.kt
    Co-authored-by: Dylan Muszel <dylan.muszel@mercadolibre.com>

--- app/src/main/java/com/p2p/data/tuttifrutti/TuttiFruttiMetadata.kt
+++ app/src/main/java/com/p2p/data/tuttifrutti/TuttiFruttiMetadata.kt
@@ -4,32 +4,5 @@ import android.os.Parcel
 import android.os.Parcelable
 import java.util.ArrayList
 
-data class TuttiFruttiMetadata(val totalRounds: Int, private val _categories: ArrayList<String>?): Parcelable{
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.createStringArrayList()
-    ) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(totalRounds)
-        parcel.writeStringList(_categories)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    val categories = _categories!!
-
-    companion object CREATOR : Parcelable.Creator<TuttiFruttiMetadata> {
-        override fun createFromParcel(parcel: Parcel): TuttiFruttiMetadata {
-            return TuttiFruttiMetadata(parcel)
-        }
-
-        override fun newArray(size: Int): Array<TuttiFruttiMetadata?> {
-            return arrayOfNulls(size)
-        }
-    }
-
-}
+@Parcelize
+data class TuttiFruttiMetadata(val totalRounds: Int, private val categories: List<String>): Parcelable
