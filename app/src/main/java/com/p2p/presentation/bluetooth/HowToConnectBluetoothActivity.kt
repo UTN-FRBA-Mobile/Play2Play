5efcac915a88dd54177f89757ff8746d1f20b217
Dylan Muszel <dylan.muszel@mercadolibre.com>
2021-05-27 20:46:49 -0300

    Changes after merge

--- app/src/main/java/com/p2p/presentation/bluetooth/HowToConnectBluetoothActivity.kt
+++ app/src/main/java/com/p2p/presentation/bluetooth/HowToConnectBluetoothActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 import com.p2p.R
 import com.p2p.presentation.base.BaseActivity
+import com.p2p.utils.getString
 import java.io.BufferedReader
 import java.io.IOException
 import java.io.InputStreamReader
@@ -15,25 +16,10 @@ class HowToConnectBluetoothActivity : BaseActivity(R.layout.activity_how_to_conn
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        findViewById<TextView>(R.id.instructions).text = readRawTextFile(baseContext, R.raw.how_to_connect_bluetooth)
+        findViewById<TextView>(R.id.instructions).text = resources
+            .openRawResource(R.raw.how_to_connect_bluetooth)
+            .getString()
         findViewById<Button>(R.id.understood_button).setOnClickListener { finish() }
-    }
-
-    fun readRawTextFile(ctx: Context, resId: Int): String? { // TODO: use what Bren did
-        val inputStream = ctx.resources.openRawResource(resId)
-        val inputreader = InputStreamReader(inputStream)
-        val buffreader = BufferedReader(inputreader)
-        var line: String?
-        val text = StringBuilder()
-        try {
-            while (buffreader.readLine().also { line = it } != null) {
-                text.append(line)
-                text.append('\n')
-            }
-        } catch (e: IOException) {
-            return null
-        }
-        return text.toString()
     }
 
     companion object {
--- app/src/main/java/com/p2p/utils/InputStreamExtensions.kt
+++ app/src/main/java/com/p2p/utils/InputStreamExtensions.kt
@@ -1,15 +1,5 @@
 package com.p2p.utils
 
-import java.io.BufferedReader
 import java.io.InputStream
 
-fun InputStream.getString(): String{
-    val reader = BufferedReader(this.reader())
-    var content: String
-    try {
-        content = reader.readText()
-    } finally {
-        reader.close()
-    }
-    return content
-}
+fun InputStream.getString() = this.bufferedReader().use { it.readText() }
