db97c9255709e1bd97c314696737a037dcaa7899
Dylan Muszel <dylan.muszel@mercadolibre.com>
2021-05-27 20:48:01 -0300

    todo fix

--- app/src/main/java/com/p2p/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/p2p/presentation/home/HomeActivity.kt
@@ -5,8 +5,8 @@ import com.p2p.R
 import com.p2p.presentation.base.BaseActivity
 import com.p2p.presentation.home.games.GamesFragment
 
-// TODO: should we ask ACCESS_COARSE_LOCATION permission? on some examples they do, maybe it's the reason of why
-//  the discovering isn't working, but we didn't investigate it so much.
+// TODO: should we ask ACCESS_COARSE_LOCATION permission on runtime? on some examples they do, maybe it's the reason
+//  of why the discovering isn't working, but we didn't investigate it so much.
 class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
