bebe278e888ed292310ec87118f3a2242e900ea4
Dylan Muszel <dylan.muszel@mercadolibre.com>
2021-06-08 23:00:21 -0300

    Visual fixes

--- app/src/main/java/com/p2p/presentation/tuttifrutti/review/TuttiFruttiReviewFragment.kt
+++ app/src/main/java/com/p2p/presentation/tuttifrutti/review/TuttiFruttiReviewFragment.kt
@@ -1,11 +1,11 @@
 package com.p2p.presentation.tuttifrutti.review
 
 import android.content.Context
-import android.util.Log
+import android.os.Bundle
 import android.view.View
-import androidx.core.os.bundleOf
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.p2p.R
 import com.p2p.model.tuttifrutti.FinishedRoundInfo
 
@@ -14,17 +14,14 @@ class TuttiFruttiReviewFragment : Fragment() {
     // TODO: Clean this
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        val finishedRoundInfo = requireNotNull(
-            arguments
-                ?.getParcelableArrayList<FinishedRoundInfo>(FINISHED_ROUND_INFO_EXTRA)
-                ?.toList()
+        val finishedRoundInfo: List<FinishedRoundInfo> = requireNotNull(
+            arguments?.getParcelableArrayList(FINISHED_ROUND_INFO_EXTRA)
         ) {
             "No FinishedRoundInfo sent. Use the TuttiFruttiReviewFragment.newInstance."
         }
-        Log.d(
-            "P2P_REVIEW",
-            finishedRoundInfo.joinToString("\n\n") { "${it.player} --> ${it.categoriesWords}" }
-        )
+        MaterialAlertDialogBuilder(requireContext())
+            .setMessage(finishedRoundInfo.joinToString("\n\n") { "${it.player} --> ${it.categoriesWords}" })
+            .show()
         // Delete this! It's awful and we should neve do this.
         activity?.findViewById<View>(R.id.activity_progress_overlay)?.isVisible = false
     }
@@ -34,7 +31,9 @@ class TuttiFruttiReviewFragment : Fragment() {
         private const val FINISHED_ROUND_INFO_EXTRA = "FINISHED_ROUND_INFO_EXTRA"
 
         fun newInstance(finishedRoundInfo: List<FinishedRoundInfo>) = TuttiFruttiReviewFragment().apply {
-            arguments = bundleOf(FINISHED_ROUND_INFO_EXTRA to finishedRoundInfo.toTypedArray())
+            arguments = Bundle().apply {
+                putParcelableArrayList(FINISHED_ROUND_INFO_EXTRA, ArrayList(finishedRoundInfo))
+            }
         }
     }
 }
