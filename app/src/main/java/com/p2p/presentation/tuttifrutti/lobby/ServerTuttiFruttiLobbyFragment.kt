f3a5a3e36712c722641b8d27d2f5367e22ecc68f
PadillaJuan <juan.padilla@despegar.com>
2021-06-24 17:30:17 -0300

    Update app/src/main/java/com/p2p/presentation/tuttifrutti/lobby/ServerTuttiFruttiLobbyFragment.kt
    Co-authored-by: Valeria Cavallaro <valecavallaro.m@gmail.com>

--- app/src/main/java/com/p2p/presentation/tuttifrutti/lobby/ServerTuttiFruttiLobbyFragment.kt
+++ app/src/main/java/com/p2p/presentation/tuttifrutti/lobby/ServerTuttiFruttiLobbyFragment.kt
@@ -14,6 +14,7 @@ class ServerTuttiFruttiLobbyFragment: BaseGameFragment<
         LobbyEvent,
         ServerTuttiFruttiLobbyViewModel,
         TuttiFruttiViewModel>() {
+        
     override val gameViewModel: TuttiFruttiViewModel by activityViewModels()
 
     override val viewModel: ServerTuttiFruttiLobbyViewModel by viewModels()
