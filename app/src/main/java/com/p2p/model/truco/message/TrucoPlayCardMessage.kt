e1702f0a75889e156b84c35bb10588d1054d7952
Dylan Muszel <dylan.muszel@mercadolibre.com>
2021-09-26 02:13:45 -0300

    Fix json

--- app/src/main/java/com/p2p/model/truco/message/TrucoPlayCardMessage.kt
+++ app/src/main/java/com/p2p/model/truco/message/TrucoPlayCardMessage.kt
@@ -4,11 +4,10 @@ import com.fasterxml.jackson.annotation.JsonTypeName
 import com.p2p.model.base.message.Message
 import com.p2p.presentation.truco.PlayedCard
 
-@JsonTypeName(value = TrucoActionMessage.TYPE)
+@JsonTypeName(value = TrucoPlayCardMessage.TYPE)
 data class TrucoPlayCardMessage(val playedCard: PlayedCard) : Message(TYPE) {
 
     companion object {
         const val TYPE = "tr_play_card"
     }
 }
-
