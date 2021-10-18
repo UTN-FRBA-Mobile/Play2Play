6f6b8f655a9c7140030f8eb47ffc9e9ecc81a97d
Dylan Muszel <dylan.muszel@mercadolibre.com>
2021-10-18 20:29:24 -0300

    Optimize

--- app/src/main/java/com/p2p/presentation/truco/TrucoViewModel.kt
+++ app/src/main/java/com/p2p/presentation/truco/TrucoViewModel.kt
@@ -24,13 +24,13 @@ import com.p2p.presentation.truco.actions.TrucoAction
 import com.p2p.presentation.truco.actions.TrucoAction.Envido
 import com.p2p.presentation.truco.actions.TrucoAction.EnvidoGoesFirst
 import com.p2p.presentation.truco.actions.TrucoAction.FaltaEnvido
+import com.p2p.presentation.truco.actions.TrucoAction.GoToDeck
 import com.p2p.presentation.truco.actions.TrucoAction.NoIDont
 import com.p2p.presentation.truco.actions.TrucoAction.RealEnvido
 import com.p2p.presentation.truco.actions.TrucoAction.Retruco
 import com.p2p.presentation.truco.actions.TrucoAction.Truco
 import com.p2p.presentation.truco.actions.TrucoAction.ValeCuatro
 import com.p2p.presentation.truco.actions.TrucoAction.YesIDo
-import com.p2p.presentation.truco.actions.TrucoAction.GoToDeck
 import com.p2p.presentation.truco.actions.TrucoActionAvailableResponses
 import com.p2p.presentation.truco.actions.TrucoGameAction
 import com.p2p.presentation.truco.envidoCalculator.EnvidoMessageCalculator
